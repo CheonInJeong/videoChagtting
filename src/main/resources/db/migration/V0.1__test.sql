79cac77632187614a02a82de3fdc065abaec64cb
chenchen <burub3@gmail.com>
2023-04-29 11:03:13 +0900

    비디오 채팅 프로젝트 초기 셋팅

---
+++ src/main/resources/db/migration/V0.1__test.sql
@@ -0,0 +1,8 @@
+-- videochatting.TEST definition
+
+CREATE TABLE videochatting.TEST (
+    TEST_ID varchar(100) NOT NULL
+)
+    ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci;
---
+++ src/test/java/com/video/videochatting/VideoChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.video.videochatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoChattingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
